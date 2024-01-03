023f7c0a101e6d27d650cc9e67ee2b5fa7273436
Gabriel <gabrielsantospds@hotmail.com>
2024-01-03 16:53:19 -0300

    test

--- src/app/home/user-data/view-data/view-data.component.ts
+++ src/app/home/user-data/view-data/view-data.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Register } from 'src/app/register';
 
 @Component({
   selector: 'app-view-data',
   templateUrl: './view-data.component.html',
   styleUrls: ['./view-data.component.css']
 })
-export class ViewDataComponent {
-
+export class ViewDataComponent implements OnInit {
+  
+  register!: Register
+  
+  ngOnInit(): void {
+    this.register = {
+      name: "Guilherme",
+      email: "guilherme@gmail.com",
+      password: "*******",
+      birthDate: "13/08/2001"
+    }
+  }
 }
